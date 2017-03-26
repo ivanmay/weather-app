33c427a4e7dfc834860244ca2e01d3b5513c9d22
ivan-may <ivan@keycrowd.co.za>
2017-03-26 12:23:43 +0200

    Create Error Modal

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,32 @@
+var React = require('react')
+
+var ErrorModal = React.createClass ({
+  getDefaultProps: function () {
+    return {
+      title: 'Error'
+    }
+  },
+  propTypes: function () {
+    title: React.propTypes.string
+    message: React.propTypes.string.isRequired
+  },
+  componentDidMount: function () {
+    var modal = new Foundation.Reveal($('#error-modal'))
+    modal.open();
+  },
+  render: function () {
+    var {title, message} = this.props;
+
+    return (
+      <div id='error-modal' className='reveal tiny text-center' data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>
+          <button className='hollow button expanded' data-close="">Okay</button>
+        </p>
+      </div>
+    )
+  }
+})
+
+module.exports = ErrorModal
