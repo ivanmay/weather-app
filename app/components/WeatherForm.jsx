f814055dbbdad7ce096de67fc2accd61dac08e12
ivan-may <ivan@keycrowd.co.za>
2017-03-26 14:52:05 +0200

    Add Styles

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref='location'/>
+          <input type="search" ref='location' placeholder='Search Weather by City'/>
           <button className='hollow button expanded'>Get Weather</button>
         </form>
       </div>
